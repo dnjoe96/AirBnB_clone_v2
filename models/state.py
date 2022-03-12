6983a82db1d06b7feba33083deaf306f5f6cb5af
Beldine-Moturi <beldinemoturi@gmail.com>
2022-03-12 23:18:55 +0300

    [fix]: Define cities attribute
    cities class attribute that works with the database storage- defines the relationship between State and City--\ncities getter attribute that works with the file storage- returns cities for the current state

--- models/state.py
+++ models/state.py
@@ -2,9 +2,27 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, DateTime
-
+from models import storage_type
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
+    if storage_type == "db":
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state",
+                              cascase="all, delete, delete-orphan")
+    else:
+        name = ""
+
+        @property
+        def cities(self):
+            """a getter attribute that returns a list of city instances
+            with state_id equal to the current State.id"""
+
+        from models import storage
+        state_cities = []
+        cities = storage.all(City)
+        for city in cities.values():
+            if city.state_id == self.id:
+                state_cities.append(city)
+        return state_cities
