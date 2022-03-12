fdd628462be7c6f4e08181421ff9bfe7e33101f3
Beldine-Moturi <beldinemoturi@gmail.com>
2022-03-12 23:26:32 +0300

    [fix]: Fix indentation

--- models/state.py
+++ models/state.py
@@ -20,10 +20,10 @@ class State(BaseModel, Base):
             """a getter attribute that returns a list of city instances
             with state_id equal to the current State.id"""
 
-        from models import storage
-        state_cities = []
-        cities = storage.all(City)
-        for city in cities.values():
-            if city.state_id == self.id:
-                state_cities.append(city)
-        return state_cities
+            from models import storage
+            state_cities = []
+            cities = storage.all(City)
+            for city in cities.values():
+                if city.state_id == self.id:
+                    state_cities.append(city)
+            return state_cities
