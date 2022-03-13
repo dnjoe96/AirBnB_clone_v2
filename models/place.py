ee798a95ad581ede36344dfaaa03d125da250997
Beldine-Moturi <beldinemoturi@gmail.com>
2022-03-13 23:13:17 +0300

    [feat]: enable switching between db_storage and file storage in amenities class

--- models/place.py
+++ models/place.py
@@ -2,9 +2,15 @@
 """ Place Module for HBNB project """
 from models import storage_type
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, Integer, String, ForeignKey, Float
+from sqlalchemy import Column, Integer, String, ForeignKey, Float, Table
 from sqlalchemy.orm import relationship
 
+
+place_amenity = Table('association', Base.metadata,
+                      Column('place_id', String(60), ForeignKey('places.id'),
+                             primary_key=True, nullable=False)
+                      Column('amenity_id', String(60), ForeignKey('amenities.id'
+                      ), primary_key=True, nullable=False))
 
 class Place(BaseModel, Base):
     """ A place to stay """
@@ -22,7 +28,8 @@ class Place(BaseModel, Base):
         longitude = Column(Float, nullable=False, default=0)
         reviews = relationship('Place', cascade='all, delete, delete-orphan',
                                backref='place')
-        #amenity_ids = []
+        amenities = relationship('Amenity', secondary=place_amenity,
+                                 viewonly=False)
     else:
         city_id = ""    # City.id
         user_id = ""    # User.id
@@ -49,3 +56,19 @@ class Place(BaseModel, Base):
                 if review.place_id == self.id:
                     place_reviews.append(review)
             return place_reviews
+
+        @property
+        def amenities(self):
+            """returns the list of Amenity instances based on the attribute
+            amenity_ids that contains all Amenity.id linked to the Place"""
+
+            return amenity_ids
+
+        @amenities.setter
+        def amenities(self, obj):
+            """handles append method for adding an Amenity.id to the attribute
+            amenity_ids"""
+
+            from models.amenity import Amenity
+            if type(obj) == Amenity:
+                amenity_ids.append(obj.id)
