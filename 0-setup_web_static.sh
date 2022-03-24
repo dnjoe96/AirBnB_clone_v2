d9dcd674bac0d0d77e09afe06dc0ed8c629148fd
dnjoe96 <donjoedbest@gmail.com>
2022-03-24 09:26:50 +0100

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -27,21 +27,11 @@ do
 	chown ubuntu:$group $file
 done
 
-sed -i '/location \// i \\tlocation /hbnb_static {\n\t\troot /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sed -i '/location \// i \\tlocation /hbnb_static {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
 sed -i '/listen 80 default_server;/ a \\trewrite ^/redirect_me https://github.com/dnjoe96 permanent;' /etc/nginx/sites-available/default
 sed -i '/listen 80 default_server;/ a \\trewrite ^/redirect_me/ https://github.com/dnjoe96 permanent;' /etc/nginx/sites-available/default
 touch /var/www/html/custom_404.html && echo "Ceci n'est pas une page" > /var/www/html/custom_404.html
 sed -i '/listen 80 default_server;/ a \\terror_page 404 /custom_404.html;' /etc/nginx/sites-available/default
 sed -i '/server_name _;/ a \\tadd_header X-Served-By $HOSTNAME;' /etc/nginx/sites-available/default
 service nginx restart
-
-Install Nginx if it not already installed
-Create the folder /data/ if it doesn’t already exist
-Create the folder /data/web_static/ if it doesn’t already exist
-Create the folder /data/web_static/releases/ if it doesn’t already exist
-Create the folder /data/web_static/shared/ if it doesn’t already exist
-Create the folder /data/web_static/releases/test/ if it doesn’t already exist
-Create a fake HTML file /data/web_static/releases/test/index.html (with simple content, to test your Nginx configuration)
-Create a symbolic link /data/web_static/current linked to the /data/web_static/releases/test/ folder. If the symbolic link already exists, it should be deleted and recreated every time the script is ran.
-Give ownership of the /data/ folder to the ubuntu user AND group (you can assume this user and group exist). This should be recursive; everything inside should be created/owned by this user/group.
-Update the Nginx configuration to serve the content of /data/web_static/current/ to hbnb_static (ex: https://mydomainname.tech/hbnb_static
+exit 0
