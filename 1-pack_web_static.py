61093d49b47698c0b8a7a7d16c77a6d04b15cbc7
dnjoe96 <donjoedbest@gmail.com>
2022-03-25 13:20:49 -0700

    1-pack_web_static.py

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -2,6 +2,7 @@
 """ Fab file for deploying static file """
 from fabric.api import run, get, put, local
 from datetime import datetime
+import os
 
 
 def do_pack():
@@ -13,7 +14,9 @@ def do_pack():
     tarFile = f"web_static_{d.year}{d.month}{d.day}{d.hour}{d.minute}{d.second}.tgz"
 
     print('Packing web_static to versions/{}'.format(tarFile))
-    local('if [ ! -d versions ]; then mkdir versions; fi')
+
+    if os.path.isdir("versions") is False:
+        local("mkdir -p versions")
     res = local(f'tar -cvzf versions/{tarFile} web_static')
     if res.failed:
         return None
