fbecc4ada3902a6c5ff21d475362a87794600e6d
dnjoe96 <donjoedbest@gmail.com>
2022-04-10 12:06:33 +0100

    2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -7,16 +7,19 @@ app.url_map.strict_slashes = False
 
 @app.route("/")
 def hello_hbnb():
+    """ view definition """
     return "Hello HBNB!"
 
 
 @app.route("/hbnb")
 def hbnb():
+    """ view definition """
     return "HBNB"
 
 
 @app.route("/c/<text>")
 def whatisc(text):
+    """ view definition """
     final_text = text.replace("_", " ")
     return f"C {final_text}"
 
