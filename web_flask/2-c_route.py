fa17a8272f23c17e3e81b41d9a8e68ae4bbc102a
dnjoe96 <donjoedbest@gmail.com>
2022-04-10 11:40:05 +0100

    2-c_route.py

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" route file """
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/")
+def hello_hbnb():
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb")
+def hbnb():
+    return "HBNB"
+
+
+@app.route("/c/<string:text>")
+def whatisc(text):
+    return f"C {text}"
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0', port=5000)
