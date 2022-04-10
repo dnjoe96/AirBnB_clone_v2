523a52bcd51d132c0f94a1449aa308343ec595f5
dnjoe96 <donjoedbest@gmail.com>
2022-04-10 11:30:52 +0100

    1-hbnb_route.py

---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" route file """
+from flask import Flask
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route("/hbnb")
+def hello_hbnb():
+    return "HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
