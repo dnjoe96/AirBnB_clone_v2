dbd7cbba0235e6d9db3f8290570019fe4910e56a
dnjoe96 <donjoedbest@gmail.com>
2022-04-10 11:14:42 +0100

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 """ route file """
-from flask import Flask
-
-app = Flask(__name__)
-app.url_map.strict_slashes = False
-
-@app.route('/')
-def index():
-    """ Index page """
-    return 'Hello HBNB!'
-
 
 if __name__ == '__main__':
+    from flask import Flask
+
+    app = Flask(__name__)
+    app.url_map.strict_slashes = False
+
+    @app.route('/')
+    def index():
+        """ Index page """
+        return 'Hello HBNB!'
+
     app.run(debug=True, host='0.0.0.0')
